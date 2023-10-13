8afb12331e177979e5869bc80c1a8d159a5d8316
Adam Lopez <adamf04@gmail.com>
2023-10-13 15:49:14 -0400

    Adding a test form to check the department routes I previously created. Also had to run npm run dev in a split terminal. Need to add that to my readme.

---
+++ resources/views/admin/department/create.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <form action="" method="post">
+                @csrf
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea type="text" name="description" class="form-control @error('description') is-invalid @enderror"></textarea>
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success mt-3">Submit</button>
+
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
