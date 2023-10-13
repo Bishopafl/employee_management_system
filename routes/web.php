1859ce16d7262a769c561fbfedbaa54308e67e1f
Adam Lopez <adamf04@gmail.com>
2023-10-13 15:48:17 -0400

    Created the department controller and the resource endpoints for that controller. Testing the endpoints routes now.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('departments', DepartmentController::class);
