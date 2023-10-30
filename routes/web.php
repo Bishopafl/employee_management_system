11f199809f776270fd4f18de299034a19c52770b
Adam Lopez <adamf04@gmail.com>
2023-10-30 19:47:02 -0400

    Moving the homepage view to inside the middleware auth group in the web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -17,18 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'], function() {
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
     Route::resource('departments', DepartmentController::class);
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
+    Route::resource('permission', PermissionController::class);
 });
 
 Auth::routes();
